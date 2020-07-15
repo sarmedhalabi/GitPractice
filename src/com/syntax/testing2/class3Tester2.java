ba75b323043d968b86a35911d7898ec7e141687f
sarmedhalabi <sarmedhalabi@gmail.com>
2020-07-14 21:11:38 -0400

    new package and class added

---
+++ src/com/syntax/testing2/class3Tester2.java
@@ -0,0 +1,10 @@
+package com.syntax.testing2;
+
+public class class3Tester2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/syntax/testing3/newClass.java
@@ -0,0 +1,10 @@
+package com.syntax.testing3;
+
+public class newClass {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
